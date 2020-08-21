f7549daa0f33df961e5cb86d2e557ac539aae591
Tom Tyler <tom.tyler@live.co.uk>
2020-08-21 10:41:33 +0100

    remove arrow functions from app.js

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ require('dotenv').config();
 const express = require("express"),
       app = express(),
       axios = require("axios"),
-      mongoose = require("mongoose"),
-      seedDB = require("./seed.js");
+      mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 
@@ -19,7 +18,7 @@ mongoose.connect(process.env.DATABASE_URL, {
     useFindAndModify:false 
 });
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     Destinations.find({}, (err, foundDestinations) => {
         if(err){
             console.log(err, "Destinations could not be found");
@@ -33,7 +32,7 @@ app.get("/:id", function(req, res){
     let id = req.params.id,
         url = "http://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + process.env.OPEN_WEATHER_KEY + "&id=" + id;
     axios.get(url)
-    .then(function(response){
+    .then((response) => {
         let sunset = new Date(response["data"].sys.sunset * 1000),
             hours = sunset.getHours(),
             minutes = `0 ${sunset.getMinutes()}`,
@@ -50,8 +49,6 @@ app.get("/:id", function(req, res){
 app.get("*", function(req, res){
     res.render("error");
 });
-
-// seedDB();
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Weather data is running ...");
